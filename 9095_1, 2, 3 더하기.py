5f168bcaa09c99358fcb057749cccfbf3ba9bd7d
kje <jeun313@gmail.com>
2021-07-27 10:02:47 +0900

    0727

--- 9095_1, 2, 3 더하기.py
+++ 9095_1, 2, 3 더하기.py
@@ -1,13 +1,13 @@
-def addn(n)
-    if n == 1
+def addn(n):
+    if n == 1:
         return 1
-    elif n == 2
+    elif n == 2:
         return 2
-    elif n == 3
+    elif n == 3:
         return 4
-    else 
+    else :
         return addn(n-1) + addn(n-2) + addn(n-3)
 
 T = int(input())
-for _ in range(T)
+for _ in range(T):
     print(addn(int(input())))
