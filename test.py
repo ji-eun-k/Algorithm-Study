7ccf6c467f453ef855ddaabee02b4a10c402d035
kje <jeun313@gmail.com>
2021-10-18 22:59:11 +0900

    1018

--- test.py
+++ test.py
@@ -22,40 +22,48 @@
 #     quick_sort(arr, r+1, end)
 
 
-def merge_sort(arr):
-    if len(arr) == 1:
-        return arr
+# def merge_sort(arr):
+#     if len(arr) == 1:
+#         return arr
+#
+#     l_arr = merge_sort(arr[:len(arr)//2])
+#     r_arr = merge_sort(arr[len(arr)//2:])
+#
+#     new_arr = [0]*len(arr)
+#     l_idx, r_idx, k = 0, 0, 0
+#     while r_idx < len(r_arr) and l_idx < len(l_arr):
+#         if l_arr[l_idx] > r_arr[r_idx]:
+#             new_arr[k] = r_arr[r_idx]
+#             k += 1
+#             r_idx += 1
+#         else:
+#             new_arr[k] = l_arr[l_idx]
+#             l_idx += 1
+#             k += 1
+#
+#     while r_idx < len(r_arr):
+#         new_arr[k] = r_arr[r_idx]
+#         k += 1
+#         r_idx += 1
+#
+#     while l_idx < len(l_arr):
+#         new_arr[k] = l_arr[l_idx]
+#         k += 1
+#         l_idx += 1
+#
+#     return new_arr
 
-    l_arr = merge_sort(arr[:len(arr)//2])
-    r_arr = merge_sort(arr[len(arr)//2:])
 
-    new_arr = [0]*len(arr)
-    l_idx, r_idx, k = 0, 0, 0
-    while r_idx < len(r_arr) and l_idx < len(l_arr):
-        if l_arr[l_idx] > r_arr[r_idx]:
-            new_arr[k] = r_arr[r_idx]
-            k += 1
-            r_idx += 1
-        else:
-            new_arr[k] = l_arr[l_idx]
-            l_idx += 1
-            k += 1
-
-    while r_idx < len(r_arr):
-        new_arr[k] = r_arr[r_idx]
-        k += 1
-        r_idx += 1
-
-    while l_idx < len(l_arr):
-        new_arr[k] = l_arr[l_idx]
-        k += 1
-        l_idx += 1
-
-    return new_arr
-
-
-my_list = [9, 4, 3, 2, 1, 6, 5]
-# quick_sort(my_list, 0, len(my_list)-1)
-print(merge_sort(my_list))
+# my_list = [9, 4, 3, 2, 1, 6, 5]
+# # quick_sort(my_list, 0, len(my_list)-1)
+# print(merge_sort(my_list))
 
 # 이분 탐색 : sort 먼저 해야함!
+
+
+def binary(n):
+    if n < 2:
+        return str(n)
+    return binary(n//2) + str(n%2)
+
+print(binary(2))
--- 완전탐색-구현-재귀/17070_파이프 옮기기 1_dp.py
+++ 완전탐색-구현-재귀/17070_파이프 옮기기 1_dp.py
@@ -1,6 +1,24 @@
 n = int(input())
 house = [list(map(int, input().split())) for _ in range(n)]
 
-check_dict = {1:(1, 3), 2:(2, 3), 3:(1, 2, 3)}
-pos_dict = {1:(0, 1), 2:(1, 0), 3:(1, 1)}
+check_dict = {0: (0, 2), 1: (1, 2), 2: (0, 1, 2)}
+pos_dict = {0: (0, 1), 1: (1, 0), 2: (1, 1)}
 ans = 0
+dp = [[[0]*3 for _ in range(n)] for _ in range(n)]
+dp[0][1][0] = 1
+for i in range(n):
+    for j in range(n):
+        for k in range(3):
+            if dp[i][j][k] and not house[i][j]:  # 해당 방향으로 파이프가 놓이는 방법이 있고, 벽이 없음
+                for x in check_dict[k]:
+                    ny = i + pos_dict[x][0]
+                    nx = j + pos_dict[x][1]
+                    if ny < n and nx < n and not house[ny][nx]:
+                        if x != 2: # 대각선이 아니라면
+                            dp[ny][nx][x] += dp[i][j][k]
+                        else: # 대각선이라면
+                            if not house[ny-1][nx] and not house[ny][nx-1]:
+                                dp[ny][nx][x] += dp[i][j][k]
+
+
+print(sum(dp[n-1][n-1]))
